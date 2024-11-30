448ab57880aa87b6af4dc54ee68d90475b618206
thomas-topway-it <topway.it@mail.com>
2024-11-30 23:55:36 +0400

    1.0

---
+++ README.md
@@ -0,0 +1,7 @@
+# ChameleonThemes
+
+Provides a simple framework to easily create sub-skins (completed with Chameleon components, scripts and styles) for the Chameleon skin.
+The extension includes additional Chameleon sub-skins/themes.
+
+Please check https://www.mediawiki.org/wiki/Extension:ChameleonThemes for the official documentation.
+
---
+++ includes/ChameleonThemesHooks.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * This file is part of the MediaWiki extension ChameleonThemes.
+ *
+ * ChameleonThemes is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * ChameleonThemes is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ChameleonThemes.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @file
+ * @ingroup extensions
+ * @author thomas-topway-it <support@topway.it>
+ * @copyright Copyright ©2024, https://wikisphere.org
+ */
+
+use MediaWiki\MediaWikiServices;
+
+class ChameleonThemesHooks {
+
+	public static function onRegistration() {
+		ChameleonThemes::initTheme();
+	}
+
+	/**
+	 * @param Title &$title
+	 * @param null $unused
+	 * @param OutputPage $output
+	 * @param User $user
+	 * @param WebRequest $request
+	 * @param MediaWiki|MediaWiki\Actions\ActionEntryPoint $mediaWiki
+	 * @return void
+	 */
+	public static function onBeforeInitialize( &$title, $unused, $output, $user, $request, $mediaWiki ) {
+	}
+
+	/**
+	 * @param Parser $parser
+	 * @return void
+	 */
+	public static function onParserFirstCallInit( $parser ) {
+		$parser->setFunctionHook( 'noparse', [ ChameleonThemes::class, 'parserFunctioNoparse' ] );
+	}
+
+	/**
+	 * @param OutputPage $outputPage
+	 * @param Skin $skin
+	 * @return void
+	 */
+	public static function onBeforePageDisplay( OutputPage $outputPage, Skin $skin ) {
+		$theme = $GLOBALS['wgChameleonThemesTheme'];
+		$outputPage->addModules( [ "ext.ChameleonThemes.$theme" ] );
+	}
+}
